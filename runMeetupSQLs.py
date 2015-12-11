bfb4f016db26cd96d94af602c4f3b152af24acfc
atillaahun <szikora.atilla@gmail.com>
2015-12-11 18:19:37 +0100

    finaFUCKingly

---
+++ runMeetupSQLs.py
@@ -0,0 +1,9 @@
+"""
+This application should use the SQL files for creating the DB.
+After the schema creation run the insertion scripts.
+List all meetup registrations for a particular user.
+List meetups which are after 2015.11.27.
+List that users who have introduction (NOT NULL).
+Keep in mind handle the exceptions if necessary.
+"""
+
