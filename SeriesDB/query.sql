f1c8e15b932e7a168ba9a50894778316c59d015f
atillaahun <szikora.atilla@gmail.com>
2015-12-14 21:41:36 +0100

    now it really works hope it stays that way til tomorrow

--- SeriesDB/query.sql
+++ SeriesDB/query.sql
@@ -2,4 +2,4 @@ SELECT Series.*, Statuses.StatusValue
 FROM Watching
 JOIN Series ON Watching.SeriesId = Series.Id
 JOIN Statuses ON Watching.StatusId = Statuses.Id
-WHERE Statuses.Id = 1;
+WHERE Statuses.Id = 1
